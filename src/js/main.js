bde55f6964f196aa17e85d6508d0b0cc889d70cc
FranciscoPonceGomez <pakopongo@gmail.com>
2018-05-20 22:19:37 -0700

    Added compositor

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import SpriteSheet from './SpriteSheet.js';
-import {loadImage, loadLevel} from './helpers.js';
+import {loadLevel} from './helpers.js';
+import {loadBackGroundSprites, loadMarioSprites} from './sprites.js';
 
 function drawBackground(background, context, sprites) {
     background.ranges.forEach(([xStart,xEnd,yStart,yEnd]) => {
@@ -9,37 +9,57 @@ function drawBackground(background, context, sprites) {
     });
 }
 
-function loadBGSprites() {
-    return loadImage('/img/tiles.png').then(image => {
-        const sprites = new SpriteSheet(image,16,16);
-        sprites.define('ground', 0, 0);
-        sprites.define('sky', 3, 23);
-        console.log('sprites loaded', sprites);
-        return sprites;
-    });
-}
-
-function loadMarioSprites() {
-    return loadImage('/img/characters.gif').then(image => {
-        const sprites = new SpriteSheet(image,16,16);
-        sprites.define('idle', 17, 3);
-        return sprites;
-    });
-}
-
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
+class Compositor {
+    constructor() {
+        this.layers = []
+    }
+
+    draw(context) {
+        this.layers.forEach(layer => {
+           layer(context); 
+        });
+    }
+}
+
+function createBackgroundLayer(backgrounds, sprites) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256;
+    buffer.height = 240;
+
+    backgrounds.forEach(background => {
+        drawBackground(background, buffer.getContext('2d'), sprites);
+    })
+
+    return function drawBackgroundLayer(context) {
+        context.drawImage(buffer, 0, 0);
+    };
+}
+
 Promise.all([
-    loadBGSprites(),
+    loadBackGroundSprites(),
     loadMarioSprites(),
     loadLevel('1-1'),
 ])
 .then(([sprites, marioSprite, level]) => {
-    console.log('level loaded', level);
-    level.backgrounds.forEach(background => {
-        drawBackground(background, context, sprites);
-    })
-    marioSprite.draw('idle', context, 64, 64);
+    const comp = new Compositor();
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+    comp.layers.push((backgroundLayer));
+
+    const pos = {
+        x: 64,
+        y: 64,
+    }
+
+    function update() {
+        comp.draw(context);
+        marioSprite.draw('idle', context, pos.x, pos.y);
+        pos.x += 2;
+        pos.y += 2;
+        requestAnimationFrame(update);
+    }
+    update();
 });
 
---
+++ src/js/sprites.js
@@ -0,0 +1,20 @@
+import {loadImage} from './helpers.js';
+import SpriteSheet from './SpriteSheet.js'
+
+export function loadBackGroundSprites() {
+    return loadImage('/img/tiles.png').then(image => {
+        const sprites = new SpriteSheet(image,16,16);
+        sprites.defineTile('ground', 0, 0);
+        sprites.defineTile('sky', 3, 23);
+        console.log('sprites loaded', sprites);
+        return sprites;
+    });
+}
+
+export function loadMarioSprites() {
+    return loadImage('/img/characters.gif').then(image => {
+        const sprites = new SpriteSheet(image,16,16);
+        sprites.define('idle', 276, 44, 16, 16);
+        return sprites;
+    });
+}
